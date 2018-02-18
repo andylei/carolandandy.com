683ccd8b877c93ae33e837b24cb9e195c72654ae
Andy Lei <andy@addepar.com>
2018-02-17 23:34:07 -0500

    most pics

--- src/storydata.js
+++ src/storydata.js
@@ -10,6 +10,7 @@ export default [
   {
     "group": "carol",
     "content": "Carol is Born",
+    "img": require('./img/carol-baby.jpg'),
     "desc": "Carol is born to Richard and Mary Berger. She joins her older siblings Christine and Alex.",
     "start": "1988-05-02",
     "loc": {"lat": 42.2008575, "lng": -88.2145259}
@@ -17,6 +18,7 @@ export default [
   {
     "group": "carol",
     "content": "The Bergers move to Minnesota.",
+    "img": require('./img/carol-mn.jpg'),
     "desc": "Money magazine consistently ranks Eden Prairie, Minnesota as one of the best places to live.",
     "start": "1988-11-01",
     "loc": {"lat": 44.8546856, "lng": -93.470786}
@@ -24,6 +26,7 @@ export default [
   {
     "group": "andy",
     "content": "Andy moves to America",
+    "img": require('./img/andy-usa.jpg'),
     "desc": "The specter of communism brings the Lei family to America . May doesn't understand why she couldn't bring her kitchen knives on the flight.",
     "start": "1992-01-14",
     "loc": {"lat": 37.6987072, "lng": -89.2360234}
@@ -31,6 +34,7 @@ export default [
   {
     "group": "carol",
     "content": "Carol moves to Wisconsin",
+    "img": require('./img/carol-bfield.jpg'),
     "desc": "GE brings the Berger family to Brookfield, Wisconsin, a beautiful town built atop a swamp.",
     "start": "1992-11-13",
     "loc": {"lat": 43.0694789, "lng": -88.1682145}
@@ -38,6 +42,7 @@ export default [
   {
     "group": "andy",
     "content": "Andy moves to Wisconsin",
+    "img": require('./img/andy-greendale.jpg'),
     "desc": "Andy's parents finish grad school and find gainful employment in Wisconsin.",
     "start": "1996-06-24",
     "loc": {"lat": 42.9405711, "lng": -87.995919}
@@ -45,6 +50,7 @@ export default [
   {
     "group": "andy",
     "content": "Andy moves to Brookfield",
+    "img": require('./img/andy-bfield.jpg'),
     "desc": "David and May buy their first house; it's in the same neighborhood as the Bergers!",
     "start": "1999-06-01",
     "loc": {"lat": 43.064098, "lng": -88.183387}
@@ -52,6 +58,7 @@ export default [
   {
     "group": "carol",
     "content": "Carol goes to college",
+    "img": require('./img/carol-umn2.jpg'),
     "desc": "University of MN, home of the gophers. She studies design.",
     "start": "2006-08-01",
     "loc": {"lat": 44.97399, "lng": -93.2277285}
@@ -59,6 +66,7 @@ export default [
   {
     "group": "andy",
     "content": "Andy goes to college",
+    "img": require('./img/andy-harvard.jpg'),
     "desc": "Harvard University, the only college in America his grandma has ever heard about.",
     "start": "2006-08-02",
     "loc": {"lat": 42.3770029, "lng": -71.11666}
