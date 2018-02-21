5e439af03c50046271088a9678686cb6b74cfa42
Andy Lei <andy@addepar.com>
2018-02-20 22:12:26 -0500

    pics and blurbs

--- src/party.js
+++ src/party.js
@@ -1,13 +1,2 @@
 import "./style.css";
 import "./party.css";
-
-let index = 0;
-const fonts = ['rl', 'ab', 'lo', 'no', 'pl'];
-
-let ele = document.getElementsByTagName('h1')[0];
-ele.addEventListener('click', function() {
-  let body = document.getElementsByTagName('body')[0];
-  body.classList.remove(fonts[index]);
-  index = (index + 1) % fonts.length;
-  body.classList.add(fonts[index]);
-});
