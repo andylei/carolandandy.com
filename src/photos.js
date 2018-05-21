a322c4eab575985deff87a13192621faee657af4
Andy Lei <andy@addepar.com>
2018-05-21 01:03:18 -0400

    story

---
+++ src/photos.js
@@ -0,0 +1,39 @@
+import "./style.css";
+import "./photos.css";
+
+import init_nav_button from "./nav";
+
+import ScrollMagic from 'ScrollMagic';
+import 'animation.gsap';
+
+function init() {
+
+  init_nav_button();
+
+  let controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({ duration: 400 })
+    .setTween("#title", { opacity: 0 }) // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
+  
+  let years = ['proposal'];
+  for (let i = 1988; i < 2018; i++) {
+    years.push(i);
+  }
+  
+  for (let i of years) {
+    let ele = `.caption.y${i}`;
+    if (!ele) {
+      continue;
+    }
+    new ScrollMagic.Scene({
+        duration: 400,
+        offset: i === 1988 ? 200 : 50,
+        triggerElement: ele
+      })
+      .setTween(ele, { opacity: 0 })
+      .addTo(controller);
+  }
+}
+
+init();
